0717b3162a7817e03e0cbb305463b9b8690b23ac
javaExactus <romulo.neves@exactus.com.br>
2025-04-15 13:07:27 -0300

    primeiro commit

---
+++ src/main/java/br/com/romulo/ms_estudo_webflux/MsEstudoWebfluxApplication.java
@@ -0,0 +1,13 @@
+package br.com.romulo.ms_estudo_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEstudoWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEstudoWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/romulo/ms_estudo_webflux/MsEstudoWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.romulo.ms_estudo_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEstudoWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
